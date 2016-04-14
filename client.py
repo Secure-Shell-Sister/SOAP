69fecf436299a2e3f4b6fb1bcd0bc1231b7906cc
karsten ari agathon <karstenaa@gmail.com>
2016-04-14 11:33:10 +0800

    bug fixes

--- client.py
+++ client.py
@@ -1,12 +1,17 @@
 __author__ = 'DickyIrwanto'
 import urllib2, bs4, sys
-
+import time
 if __name__ == '__main__':
     url = 'localhost'
     if len(sys.argv) > 1:
         url = sys.argv[1]
+    time_start = time.time()
     Resp = urllib2.urlopen("http://"+url+":8888/Security")
-    soup = bs4.BeautifulSoup(Resp,"lxml")
-    soup = soup.findAll('datas')
-    for x in soup:
-        print x
+    try:
+        soup = bs4.BeautifulSoup(Resp,"lxml")
+        soup = soup.findAll('datas')
+        for x in soup:
+            print x
+    except:
+        print Resp.read()
+        print time.time() - time_start, 'seconds'
